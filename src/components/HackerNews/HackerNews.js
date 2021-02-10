e734bb02f8440d5f2a91fcba3261d6bd0405e4e0
Wade Pate <wadejp8@gmail.com>
2021-02-10 08:22:28 -0700

    Added redux & functionality to site

--- src/components/HackerNews/HackerNews.js
+++ src/components/HackerNews/HackerNews.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { requestArticles } from '../../ducks/hackerNewsReducer'
 import Card from './../shared/Card/Card';
 import Loading from './../shared/Loading/Loading';
 
 class HackerNews extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { articles: [], loading: true }
+  componentDidMount(){
+    this.props.requestArticles()
   }
 
   render() {
-    const articles = this.state.articles.map((article => <Card key={article.id} article={article} />))
+    const articles = this.props.articles.map(( article ) => <Card key={ article.id } article={ article } /> )
     return (
       <div className='news-container'>
-        <img style={styles.logo} src="./hackerNews.jpeg" alt="" />
-        {this.state.loading ? <Loading /> : <div>{articles}</div>}
+        <img style={ styles.logo } src="./hackerNews.jpeg" alt="" />
+        { this.props.loading ? <Loading /> : <div>{ articles }</div>}
       </div>
     )
   }
 }
 
-export default HackerNews;
+function mapStateToProps(state){
+  // console.log(state)
+  return state.hackerNews
+}
+
+export default connect(
+  mapStateToProps,
+  { requestArticles }
+)(HackerNews)
 
 
 const styles = {
